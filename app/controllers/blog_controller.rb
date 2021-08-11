ba09c70b020b78e454a47600034d19e3cd1e2b35
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-08-11 10:56:50 -0700

    index and show working

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,2 +1,12 @@
 class BlogController < ApplicationController
+    # index makes an active record call to get all the blog posts
+    def index
+        @blog = Blog.all
+    end
+
+    # show displays one blog post
+    def show
+        @post = Blog.find(params[:id])
+    end
+    
 end
